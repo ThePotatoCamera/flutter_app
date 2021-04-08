fccd8a47e5c4aacf7bca484538c6cfed092a24a2
Alejandro <alex0xela@outlook.com>
2021-04-08 11:09:01 +0200

    Fixes para build

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/src/app.dart';
 
-void main () {
+import 'package:flutter_app/src/pages/contador_page.dart';
+// import 'package:flutter_app/src/pages/home_page.dart';
 
-  runApp(new MyApp());
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplicación de componentes',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Componentes'),
+          ),
+          body: ContadorPage()
+      ),
+    );
+  }
 }
